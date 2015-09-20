f697cd9bb1e29637552bb39fd5a2bd5aada46321
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-19 19:05:12 -0600

    fix tid checker

--- pokemon/gen2/tools/tid.py
+++ pokemon/gen2/tools/tid.py
@@ -1,13 +1,13 @@
 while True:
   tid = int(input("What is your TID? "))
-  
+
   if tid < 55296:
     print("Not a valid TID. Reset!")
-  elif tid >= 55296 or tid <= 55551:
+  elif tid >= 55296 and tid <= 55551:
     print("Valid TID! D8")
-  elif tid >= 55552 or tid <= 55807:
+  elif tid >= 55552 and tid <= 55807:
     print("Valid TID! D9")
-  elif tid >= 63488 or tid <= 63743:
+  elif tid >= 63488 and tid <= 63743:
     print("Valid TID! F8")
-  elif tid >= 63744 or tid <= 63999:
+  elif tid >= 63744 and tid <= 63999:
     print("Valid TID! F9")
