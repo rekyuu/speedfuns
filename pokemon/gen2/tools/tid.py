b18683a4feb9d03ac8898940e86ef62b912febe6
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-19 20:48:04 -0600

    added colors

--- pokemon/gen2/tools/tid.py
+++ pokemon/gen2/tools/tid.py
@@ -1,13 +1,16 @@
+from colorama import init, Fore
+init()
+
 while True:
   tid = int(input("What is your TID? "))
 
   if   tid >= 55296 and tid <= 55551:
-    print("Valid TID! D8")
+    print(Fore.GREEN + "Valid TID!", Fore.RESET + "D8")
   elif tid >= 55552 and tid <= 55807:
-    print("Valid TID! D9")
+    print(Fore.GREEN + "Valid TID!", Fore.RESET + "D9")
   elif tid >= 63488 and tid <= 63743:
-    print("Valid TID! F8")
+    print(Fore.GREEN + "Valid TID!", Fore.RESET + "F8")
   elif tid >= 63744 and tid <= 63999:
-    print("Valid TID! F9")
+    print(Fore.GREEN + "Valid TID!", Fore.RESET + "F9")
   else:
-    print("Not a valid TID, sorry! Reset!")
+    print(Fore.RED + "Not a valid TID, sorry! Reset!" + Fore.RESET)
